562d07e972c3f1657091ae94856c1096b09a1ae9
neong83 <neong83@hotmail.com>
2020-04-22 13:51:02 -0700

    solution for ex003

--- ex003_length_of_the_longest_substring.py
+++ ex003_length_of_the_longest_substring.py
@@ -1,3 +1,44 @@
+# Third attemp:
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest_length = 0
+        non_repeated_string = ""
+        
+        for c in s:
+            
+            if c in non_repeated_string:
+                non_repeated_string = non_repeated_string.split(c)[1]
+                
+            non_repeated_string += c
+            longest_length = result if (result := len(non_repeated_string)) > longest_length else longest_length
+            
+            # max() is better way to visual what the code is doing
+            # but it take additional 20ms to allocate the function and get its result
+            #longest_length = max(len(non_repeated_string), longest_length)
+            
+        return longest_length
+
+# Second attemp:
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest_length = 0
+        non_repeated_string = list()
+        
+        for c in s:
+            
+            if c in non_repeated_string:
+                # located the index of the duplicate character
+                # move cursor to next character
+                # reset the substring with what is left behind
+                non_repeated_string = non_repeated_string[non_repeated_string.index(c) + 1:]
+            
+            non_repeated_string.append(c)
+            longest_length = max(len(non_repeated_string), longest_length)
+            
+        return longest_length
+                
+
+# First attemp:
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         string_length = len(s)
